636c6b075b4cef220592f6e54b9003c9c581221c
Anthony <a.shinard12@gmail.com>
2023-10-15 10:37:21 -0700

    Add new grid button and event listener

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,8 @@
 function changeColor(e){
-    document.body.style.backgroundColor = "black";
+    e.target.style.backgroundColor = "black";
+    console.log(e.target.style.backgroundColor);
 }
+
 function makeGrid(boxNum){
     for(let i = 0; i < boxNum; i++)
     {
@@ -14,9 +16,29 @@ function makeGrid(boxNum){
     });
 }
 
+function newGrid(){
+    let answer = prompt("How many boxes on each side?");
+    console.log(answer);
+
+    let sides = parseInt(answer);
+
+    while(!(sides instanceof Number))
+    {
+        sides = prompt("Please enter a number. How many boxes per side?");
+        console.log(sides);
+    }
+
+    square.forEach(square => square.remove());
+    makeGrid(sides * sides);
+    square.forEach(square => square.addEventListener("mouseover", changeColor));
+}
+
 const etch = document.getElementById("container");
 
 makeGrid(256);
 
 const square = document.querySelectorAll(".box");
 square.forEach(square => square.addEventListener("mouseover", changeColor));
+
+const size = document.getElementById("change");
+size.addEventListener("click", newGrid);
